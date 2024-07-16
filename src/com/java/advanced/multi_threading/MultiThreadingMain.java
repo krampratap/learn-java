08be65d018af660a252348a5c897d5bab761a42e
Rampratap Kasala <53807003+krampratap@users.noreply.github.com>
2024-07-16 19:39:55 +0530

    Thread name

--- src/com/java/advanced/multi_threading/MultiThreadingMain.java
+++ src/com/java/advanced/multi_threading/MultiThreadingMain.java
@@ -3,8 +3,11 @@ package com.java.advanced.multi_threading;
 public class MultiThreadingMain {
     public static void main(String[] args) {
         System.out.println("Main thread");
-        Thread1 t1 = new Thread1();
+        Thread1 t1 = new Thread1("First Thread");
+
+        //t1.setDaemon(true);
         t1.start(); //Start is not called immediately.. it is an asynchronous method and returns immediately
+
         System.out.println("Main thread closing");
     }
 }
--- src/com/java/advanced/multi_threading/Thread1.java
+++ src/com/java/advanced/multi_threading/Thread1.java
@@ -1,11 +1,16 @@
 package com.java.advanced.multi_threading;
 
 public class Thread1 extends Thread{
+
+    public Thread1(String threadName){
+        super(threadName);
+    }
+
     @Override
     public void run(){
         for(int i=0;i<5;i++)
         {
-            System.out.println("Inside Thread 1 - "+i);
+            System.out.println("Inside Thread 1 - "+i+"thread name can be accessed by : "+Thread.currentThread().getName());
         }
     }
 }

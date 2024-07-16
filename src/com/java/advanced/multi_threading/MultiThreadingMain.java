a4ceafea7a80a05527015103bd8cc46115e4cf61
Rampratap Kasala <53807003+krampratap@users.noreply.github.com>
2024-07-16 19:44:27 +0530

    Thread using Runnable Functional Interface

--- src/com/java/advanced/multi_threading/MultiThreadingMain.java
+++ src/com/java/advanced/multi_threading/MultiThreadingMain.java
@@ -3,10 +3,12 @@ package com.java.advanced.multi_threading;
 public class MultiThreadingMain {
     public static void main(String[] args) {
         System.out.println("Main thread");
-        Thread1 t1 = new Thread1("First Thread");
+        Thread1 firstThread = new Thread1("First Thread");
+        //firstThread.setDaemon(true);
+        firstThread.start(); //Start is not called immediately.. it is an asynchronous method and returns immediately
 
-        //t1.setDaemon(true);
-        t1.start(); //Start is not called immediately.. it is an asynchronous method and returns immediately
+        Thread secondThread = new Thread(new Thread2(),"Second Thread");
+        secondThread.start();
 
         System.out.println("Main thread closing");
     }
